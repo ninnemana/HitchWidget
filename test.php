6ec5f2a483783f25621ea24580183a89854f1a67
ninnemana <ninnemana@gmail.com>
2012-11-20 15:14:26 -0600

    reorganizing

--- test.php
+++
@@ -1,7 +0,0 @@
-<a href="test.php?style=default">Default Style</a> | 
-<a href="test.php?style=darkBlue">Dark Blue</a>
-
-
-<div id="configurator" hitchdata="html"></div>
-<script type="text/javascript" src="http://dev.hitchguys.biz/api_dev/configurator.js"></script>
-
